b1b26c7419a69a6920c609d6a0475ae3c00d4a1b
LeoJuric <109168277+LeoJuric@users.noreply.github.com>
2022-07-24 17:54:12 +0200

    Update main.js

--- umivaonici/main.js
+++ umivaonici/main.js
@@ -76,3 +76,19 @@ $('#animacijabtn').click(function() {
 $('#animacijabtn').click(function() {
     document.getElementById('dash2').beginElement();
 })
+
+function setTextAnimation(delay, duration, strokeWidth, timingFunction, strokeColor,repeat) {
+    let paths = document.getElementsByClassName("path");
+    let mode=repeat?'infinite':'forwards'
+    for (let i = 0; i < paths.length; i++) {
+        const path = paths[i];
+        const length = path.getTotalLength();
+        path.style["stroke-dashoffset"] = `${length}px`;
+        path.style["stroke-dasharray"] = `${length}px`;
+        path.style["stroke-width"] = `${strokeWidth}px`;
+        path.style["stroke"] = `${strokeColor}`;
+        path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+        path.style["animation-delay"] = `${i * delay}s`;
+    }
+}
+setTextAnimation(0.1,1.7,1,'linear','#131111',false);
